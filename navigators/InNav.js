36c136c32bcc20c0520e895a021d4f78c40b80c7
걷는사람 정정훈 <92363576+walkerJung@users.noreply.github.com>
2022-02-03 23:37:08 +0900

    2022-02-03 23:37

--- navigators/InNav.js
+++ navigators/InNav.js
@@ -1,12 +1,23 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import { BLACK_COLOR } from "../colors";
+import Detail from "../screens/Detail";
 import Home from "../screens/Home";
 
 const Nav = createNativeStackNavigator();
 
 const InNav = () => (
-  <Nav.Navigator>
-    <Nav.Screen name="Home" component={Home} />
+  <Nav.Navigator
+    screenOptions={{
+      presentation: "modal",
+      headerTintColor: "white",
+      headerStyle: {
+        backgroundColor: BLACK_COLOR,
+      },
+    }}
+  >
+    <Nav.Screen name="코인" component={Home} />
+    <Nav.Screen name="Detail" component={Detail} />
   </Nav.Navigator>
 );
 
